6f11dd622198bf878eeacf0a4c60db0f7a6c45e1
FelipheTG24 <feliphaogomes@gmail.com>
2024-12-05 19:15:27 -0300

    atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ import { api } from "./api";
 interface TaskProps {
   id: string;
   description: string;
-  date: string;
-  status: boolean;
+  dataHoraPagamento: string;
+  statusPagamento: boolean;
+  meioPagamento: string,
+  valores_id: string,
+
 }
 
 export default function Home() {
@@ -17,36 +20,36 @@ export default function Home() {
   const dateRef = useRef<HTMLInputElement | null>(null)
 
   // Inicializa lista de tarefas da página como lista vazia
-  const [tasks, setTasks] = useState<TaskProps[]>([])
+  const [pagamento, setPagamento] = useState<TaskProps[]>([])
 
-  // Ao renderizar a página, chama a função "readTasks"
+  // Ao renderizar a página, chama a função "readPagamento"
   useEffect(() => {
-    readTasks();
+    readPagamento();
   }, [])
 
   // Busca as tarefas no banco de dados via API
-  async function readTasks() {
-    const response = await api.get("/tasks")
+  async function readPagamento() {
+    const response = await api.get("/pagamento")
     console.log(response.data)
-    setTasks(response.data)
+    setPagamento(response.data)
   }
 
   // Cria uma nova tarefa
   async function createTask(event: FormEvent) {
     event.preventDefault()
-    const response = await api.post("/tasks", {
+    const response = await api.post("/pagamento", {
       description: descriptionRef.current?.value,
       date: dateRef.current?.value
     }) 
-    setTasks(allTasks => [...allTasks, response.data])
+    setPagamento(allPagamento => [...allPagamento, response.data])
   }
 
   // Deleta uma tarefa
   async function deleteTask(id: string){
     try{
-      await api.delete("/tasks/" + id)
-      const allTasks = tasks.filter((task) => task.id !== id)
-      setTasks(allTasks)
+      await api.delete("/pagamento/" + id)
+      const allPagamento = pagamento.filter((task) => task.id !== id)
+      setPagamento(allPagamento)
     }
     catch(err){
       alert(err)
@@ -55,11 +58,11 @@ export default function Home() {
 
   async function setTaskDone(id:string) {
     try {
-      await api.put("/tasks/" + id, {
+      await api.put("/pagamento/" + id, {
         status: true,
       })
-      const response = await api.get("/tasks")
-      setTasks(response.data)
+      const response = await api.get("/pagamento")
+      setPagamento(response.data)
     }
     catch(err){
       alert(err)
@@ -86,11 +89,11 @@ export default function Home() {
         </section>
         <section className="mt-5 flex flex-col">
 
-          {tasks.map((task) => (
+          {pagamento.map((task) => (
             <article className="w-full bg-slate-200 text-slate-800 p-2 mb-4 rounded relative hover:bg-sky-300" key={task.id}>
               <p>{task.description}</p>
-              <p>{new Date(task.date_task).toLocaleDateString()}</p>
-              <p>{task.status.toString()}</p>
+              <p>{new Date(task.dataHoraPagamento).toLocaleDateString()}</p>
+              <p>{task.statusPagamento.toString()}</p>
 
 
               <button className="flex absolute right-10 -top-2 bg-green-600 w-7 h-7 items-center justify-center text-slate-200" onClick={() => setTaskDone(task.id)}><FiCheck></FiCheck></button>
